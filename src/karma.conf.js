6b0bef4a319bb428717bbc3171de392ff710d585
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2019-04-29 11:06:17 +0200

    #1 remove references to leo

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/leo-app'),
+      dir: require('path').join(__dirname, '../coverage/events-and-sporttables-app'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
